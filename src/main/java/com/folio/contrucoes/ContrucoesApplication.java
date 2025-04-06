4e6ba302fd96b8fb6946755636d9ee5dc2ec5f23
klinton lee <klintonlee92@gmail.com>
2025-04-06 17:27:44 -0300

    chore: adiciona estrutura inicial do projeto integrador univesp

---
+++ src/main/java/com/folio/contrucoes/ContrucoesApplication.java
@@ -0,0 +1,13 @@
+package com.folio.contrucoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrucoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrucoesApplication.class, args);
+	}
+
+}
